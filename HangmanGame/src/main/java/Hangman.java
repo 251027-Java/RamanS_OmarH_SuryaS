4b74b32994c45daac04e7a07d1fd9e032fba0400
snsorl <suryasolaiappan@gmail.com>
2025-10-29 14:48:37 -0500

    adding array of false values

--- HangmanGame/src/main/java/Hangman.java
+++ HangmanGame/src/main/java/Hangman.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Hangman {
     public static void main(String[] args) {
+        //word
+        String word = "Dinosaur";
+
+        // empty array with all false values
+        boolean[] wordBoolean = new boolean[word.length()];
+        System.out.println(Arrays.toString(wordBoolean));
+
         Scanner scanner = new Scanner(System.in);
 
-        scanner.close()
+        // We just need to ask the user for the letter and if they guess correctly update the
+        // array value to true.  We can print an array of underscores and replace correctly guessed
+        // letters with X.
+
+        scanner.close();
     }
 }
