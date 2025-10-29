36ebb27a6c6a9f3df6617bec826ecaac4e36888f
raman-shres <123122990+raman-shres@users.noreply.github.com>
2025-10-29 18:42:26 -0500

    brand new hangman

--- HangmanGame/src/main/java/Hangman.java
+++ HangmanGame/src/main/java/Hangman.java
@@ -1,66 +1,53 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Hangman {
-    public static void main(String[] args) {
-        HashMap<Character, Boolean> correctLetters = new HashMap<Character, Boolean>();
-        String word = "apple";
-        for(char c: word.toCharArray()) {
-            correctLetters.put(c, true);
-        }
-        System.out.println(correctLetters);
-
+    public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
+        String word = "theasauraus";
+        String[] letters = word.split("");
+        String[] dashes = new String[letters.length];
 
+        for (int i = 0; i < dashes.length; i++) {
+            dashes[i] = "_";
+        }
+        boolean hangmanCompleted = false;
         int count = 0;
-        int correctGuesses = 0;
-        boolean correctGuess = false;
+        boolean correctLetter = false;
+        System.out.println("This is how many letters the word has: " + Arrays.toString(dashes));
+        System.out.println("You have a total of " + (dashes.length + 1) + " guesses left.");
 
         do {
-            System.out.print("Enter your guess: ");
-            char guessLetter = scanner.next().charAt(0);
-            if(word.contains(String.valueOf(guessLetter))) {
-                correctLetters.put(guessLetter, true);
-                correctGuesses++;
+            System.out.print("Enter your guess for the next letter: ");
+            word = scanner.next();
+            for (int i = 0; i < letters.length; i++) {
+                if (letters[i].equals(word)) {
+                    dashes[i] = letters[i];
+                    correctLetter = true;
+                }
+            }
+            if (correctLetter) {
+                System.out.println("That was a correct letter!");
+                System.out.println("This is your current status: " + Arrays.toString(dashes));
+                System.out.println("");
             }
             else {
-                System.out.println("The letter " + guessLetter + " is not in the word!");
+                System.out.println("That was incorrect!");
+                System.out.println("This is your current status: " + Arrays.toString(dashes));
+                System.out.println("");
                 count += 1;
-                System.out.println("You have gussed " + count + " time(s) and you have " + (correctLetters.size() + 2 - count) + " guesses left!");
             }
-            if(correctGuesses==correctLetters.size()){
-                System.out.println("You've guessed all of the letters correctly");
-                correctGuess = true;
+            correctLetter = false;
+            if (Arrays.equals(letters, dashes)) {
+                System.out.println("You won!");
+                System.out.println("The correct word was: " + Arrays.toString(dashes));
                 break;
             }
+        } while (count < dashes.length + 1);
 
-            System.out.println("Do you know what the word is? Enter Yes or No");
-            String ans = scanner.next();
+        System.out.println("You lost!");
+        System.out.println("The correct word was: " + Arrays.toString(letters));
+        System.out.println("This was your guess: " + Arrays.toString(dashes));
 
-            if (ans.equals("Yes")) {
-                System.out.println("Enter what you think the word is: ");
-                ans = scanner.next();
-                if (ans.equals(word)) {
-                    correctGuess = true;
-                    if (correctGuess) {
-                        break;
-                    }
-                }
-                else {
-                    System.out.println("Wrong Guess! Try again");
-                    continue;
-                }
-            }
-            else {
-                continue;
-            }
-        } while(count <= (correctLetters.size() + 2) || !correctGuess);
-        if (correctGuess) {
-            System.out.println("You win! The correct word was " + word);
-        }
-        else {
-            System.out.println("You lose! The correct word was " + word);
-        }
-
-        scanner.close();
     }
 }
