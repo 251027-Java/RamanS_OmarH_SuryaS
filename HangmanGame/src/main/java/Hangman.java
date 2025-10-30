83f7b0e2491ffece13c5a721272be86a9e3bf19c
raman-shres <123122990+raman-shres@users.noreply.github.com>
2025-10-30 09:39:22 -0500

    updated hangman with a counter that has a max of 7 guesses

--- HangmanGame/src/main/java/Hangman.java
+++ HangmanGame/src/main/java/Hangman.java
@@ -1,70 +1,58 @@
-import java.util.Objects;
 import java.util.Scanner;
 import java.util.Arrays;
 
 public class Hangman {
     public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
-        String word = "thesaurus";
-        String guessedLetter = "";
+        String word = "omar";
         String[] letters = word.split("");
         String[] dashes = new String[letters.length];
 
-
-        Arrays.fill(dashes, "_");
-        int numberOfGuesses = dashes.length+2;
-
+        for (int i = 0; i < dashes.length; i++) {
+            dashes[i] = "_";
+        }
         boolean hangmanCompleted = false;
         int count = 0;
         boolean correctLetter = false;
         System.out.println("This is how many letters the word has: " + Arrays.toString(dashes));
-        System.out.println("You have a total of " + numberOfGuesses + " guesses left.");
-
+        int guessNum = 7;
+        System.out.println("You have a total of " + guessNum + " guesses left.");
         do {
-
-            System.out.print("\nEnter your guess for the next letter: ");
-
-            guessedLetter = scanner.next();
-
-            boolean enteredElseIf = false;
+            System.out.print("Enter your guess for the next letter: ");
+            word = scanner.next();
             for (int i = 0; i < letters.length; i++) {
-                if (letters[i].equals(guessedLetter) && !dashes[i].equals(letters[i])) {
+                if (letters[i].equals(word)) {
                     dashes[i] = letters[i];
                     correctLetter = true;
-                }
-                else if (letters[i].equals(guessedLetter) && !enteredElseIf){
-                    enteredElseIf = true;
-                    System.out.println("You've already guessed that letter.");
-                    System.out.println("You have "+ numberOfGuesses +" guesses left.");
-                    correctLetter = true;
-                }
-                else{
-                    correctLetter = false;
                 }
             }
             if (correctLetter) {
                 System.out.println("That was a correct letter!");
+                System.out.println("You have a total of " + guessNum + " guesses left.");
                 System.out.println("This is your current status: " + Arrays.toString(dashes));
+                System.out.println("");
             }
             else {
-                numberOfGuesses--;
                 System.out.println("That was incorrect!");
+                guessNum -= 1;
+                System.out.println("You have a total of " + guessNum + " guesses left.");
                 System.out.println("This is your current status: " + Arrays.toString(dashes));
+                System.out.println("");
+                count += 1;
             }
-            System.out.println("You have "+ numberOfGuesses +" guesses left.");
             correctLetter = false;
             if (Arrays.equals(letters, dashes)) {
-                System.out.println("\nYou won!");
-                System.out.println("The correct word was: " + Arrays.toString(dashes));
+                System.out.println("You won!");
+                System.out.println("The correct word was: " + Arrays.toString(letters));
+                System.out.println("This was your guess: " + Arrays.toString(dashes));
                 break;
             }
-
-        } while (numberOfGuesses > 0);
-
-        if(!Arrays.equals(letters, dashes)) {
+        } while (guessNum != 0);
+        if (guessNum == 0) {
             System.out.println("You lost!");
             System.out.println("The correct word was: " + Arrays.toString(letters));
             System.out.println("This was your guess: " + Arrays.toString(dashes));
         }
+
     }
 }
